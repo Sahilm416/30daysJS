4a3256592e77a7975fb3d141ba2f1efc180507f3
sahil <sahilmulani501@gmail.com>
2024-09-19 01:12:52 +0530

    feat: peek within timeline

--- day_11/script.js
+++ day_11/script.js
@@ -5,6 +5,7 @@ const player_speed = document.querySelector(".player_speed");
 const progress_container = document.querySelector(".progress");
 const progress = document.querySelector(".progress > .timeline");
 const main_container = document.querySelector("main");
+const seek_window = document.querySelector(".seek");
 
 const skip = document.querySelector(".skip");
 const back = document.querySelector(".back");
@@ -20,8 +21,6 @@ const full_screen = document.querySelector(".full_screen");
 full_screen.addEventListener("click", (e) => {
   player.requestFullscreen();
 });
-
-console.dir(player);
 
 let is_mousedown = false;
 
@@ -50,12 +49,34 @@ const handle_speed = (e) => {
 };
 
 const handle_seek = (e) => {
-  console.dir(e.target);
   const rect = progress_container.getBoundingClientRect();
   click_position = e.clientX - rect.left;
   click_percentage = (click_position / rect.width) * 100;
   progress.style.width = `${click_percentage}%`;
   player.currentTime = player.duration * (click_percentage / 100);
+};
+
+const handle_peek = (e) => {
+  seek_window.style.display = "block";
+  const rect = progress_container.getBoundingClientRect();
+  const seek_window_width = seek_window.getBoundingClientRect().width;
+
+  // Calculate the position
+  let position = e.clientX - rect.left - seek_window_width / 2;
+
+  // Ensure the seek window doesn't overflow the container on the left or right
+  if (position < 0) {
+    position = 0; // Prevent overflow to the left
+  } else if (position + seek_window_width > rect.width) {
+    position = rect.width - seek_window_width; // Prevent overflow to the right
+  }
+
+  seek_window.style.left = `${position}px`;
+
+  const seek_percentage =
+    ((position + seek_window_width / 2) / rect.width) * 100;
+
+  seek_window.currentTime = (seek_window.duration * seek_percentage) / 100;
 };
 
 play_button.addEventListener("click", handle_play);
@@ -70,6 +91,11 @@ progress_container.addEventListener("mousedown", (e) => (is_mousedown = true));
 document.addEventListener("mouseup", () => (is_mousedown = false));
 // progress_container.addEventListener("mouseout", () => (is_mousedown = false));
 progress_container.addEventListener("mousemove", (e) => {
+  handle_peek(e);
   if (!is_mousedown) return;
   handle_seek(e);
+});
+
+progress_container.addEventListener("mouseout", () => {
+  seek_window.style.display = "none";
 });
